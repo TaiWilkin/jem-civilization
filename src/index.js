29766c54973fbe30084894e54841cfde769fdd03
Theresa Wilkin <theresawilkin@yahoo.com>
2017-07-12 17:15:24 -0400

    all

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+document.title = "Jem's Civilization";
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
